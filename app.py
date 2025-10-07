a55c00f9b285537787da5a48a1e08bc2a492aebe
PiyushPatil017 <patil298473@gmail.com>
2025-10-07 20:10:13 +0530

    this commit will add player comparison screen

--- app.py
+++ app.py
@@ -118,54 +118,92 @@ elif st.session_state.screen in ['team_screen','team_vs_team_screen']:
             st.warning('Please select different team')
 
 # If player option is chose this will be displayed on screen
-elif st.session_state.screen == 'player_screen':
-    player = st.session_state.player
-    st.title(player)
-    first_name, *others, last_name = player.split(" ")
-    st.page_link('https://en.wikipedia.org/wiki/{}_{}'.format(first_name,last_name),label=f'About {player}')
+elif st.session_state.screen in ['player_screen','player_compare_screen']:
+    if st.session_state.screen == 'player_screen':
+        player = st.session_state.player
+        st.title(player)
+        first_name, *others, last_name = player.split(" ")
+        st.page_link('https://en.wikipedia.org/wiki/{}_{}'.format(first_name,last_name),label=f'About {player}')
 
-    # player obtained from selectbox contains full name while our dataset has short name. so we send short name in request
-    with open('players_names.json') as rf:
-        data = json.load(rf)
-    player = data[player]
+        # player obtained from selectbox contains full name while our dataset has short name. so we send short name in request
+        with open('players_names.json') as rf:
+            data = json.load(rf)
+        player = data[player]
 
-    # Send API Request
-    response = requests.get('http://127.0.0.1:7000/player_record',params = {'player':player}).json()
+        # Send API Request
+        response = requests.get('http://127.0.0.1:7000/player_record',params = {'player':player}).json()
 
-    # create DataFrame for Batting
-    st.subheader('Batting Statistics')
-    overall_batting = pd.DataFrame(response['Batting']['Overall Record'], index = [0])
-    overall_batting.rename(index = {0:'Overall'}, inplace = True)
-    season_batting = pd.DataFrame(response['Batting']['Season']).T
-    season_batting.sort_index(ascending=False,inplace = True)
-    df_batting =pd.concat([overall_batting,season_batting])
-    df_batting = df_batting.fillna('-')
-    st.dataframe(df_batting)
+        # create DataFrame for Batting
+        st.subheader('Batting Statistics')
+        overall_batting = pd.DataFrame(response['Batting']['Overall Record'], index = [0])
+        overall_batting.rename(index = {0:'Overall'}, inplace = True)
+        season_batting = pd.DataFrame(response['Batting']['Season']).T
+        season_batting.sort_index(ascending=False,inplace = True)
+        df_batting =pd.concat([overall_batting,season_batting])
+        df_batting = df_batting.fillna('-')
+        st.dataframe(df_batting)
 
-    # visualize the Batting records
-    btn_bat_viz = st.button('Visualize Batting Stats')
-    if btn_bat_viz:
-        temp_batting = df_batting.iloc[1:].sort_index(ascending=True)
-        fig = px.bar(temp_batting, x=temp_batting.index, y='Runs', text_auto=True, labels={'index': 'Season'},
-                     title='Runs Scored in Each Season',height=600,width = 700)
-        st.plotly_chart(fig)
+        # visualize the Batting records
+        btn_bat_viz = st.button('Visualize Batting Stats')
+        if btn_bat_viz:
+            temp_batting = df_batting.iloc[1:].sort_index(ascending=True)
+            fig = px.bar(temp_batting, x=temp_batting.index, y='Runs', text_auto=True, labels={'index': 'Season'},
+                         title='Runs Scored in Each Season',height=600,width = 700)
+            st.plotly_chart(fig)
 
-    st.divider()
-    # create DataFrame for Bowling
-    st.subheader('Bowling Statistics')
-    overall_bowling = pd.DataFrame(response['Bowling']['Overall Record'], index=[0])
-    overall_bowling.rename(index={0: 'Overall'}, inplace=True)
-    season_bowling = pd.DataFrame(response['Bowling']['Season']).T
-    season_bowling.sort_index(ascending=False, inplace=True)
-    df_bowling = pd.concat([overall_bowling, season_bowling])
-    df_bowling = df_bowling.fillna('-')
-    st.dataframe(df_bowling)
+        # create DataFrame for Bowling
+        st.divider()
+        st.subheader('Bowling Statistics')
+        overall_bowling = pd.DataFrame(response['Bowling']['Overall Record'], index=[0])
+        overall_bowling.rename(index={0: 'Overall'}, inplace=True)
+        season_bowling = pd.DataFrame(response['Bowling']['Season']).T
+        season_bowling.sort_index(ascending=False, inplace=True)
+        df_bowling = pd.concat([overall_bowling, season_bowling])
+        df_bowling = df_bowling.fillna('-')
+        st.dataframe(df_bowling)
 
-    # visualize the Bowling records
-    btn_bowl_viz = st.button('Visualize Bowling Stats')
-    if btn_bowl_viz:
-        # bar chart for runs in each season
-        temp_bowling = df_bowling.iloc[1:].sort_index(ascending=True)
-        fig = px.bar(temp_bowling, x=temp_bowling.index, y='Wickets', text_auto=True, labels={'index': 'Season'},
-                     title='Wickets Taken in Each Season', height=600, width=700)
+        # visualize the Bowling records
+        btn_bowl_viz = st.button('Visualize Bowling Stats')
+        if btn_bowl_viz:
+            # bar chart for runs in each season
+            temp_bowling = df_bowling.iloc[1:].sort_index(ascending=True)
+            fig = px.bar(temp_bowling, x=temp_bowling.index, y='Wickets', text_auto=True, labels={'index': 'Season'},
+                         title='Wickets Taken in Each Season', height=600, width=700)
+            st.plotly_chart(fig)
+
+    # add option to compare with other players
+    st.sidebar.divider()
+    player_compare_list = st.sidebar.multiselect('Select to player to compare stats', options=players,
+                                                 key='compare_player', max_selections=3)
+    player_compare_list.insert(0, st.session_state.player)
+    player_compare_btn = st.sidebar.button('Compare')
+    if player_compare_btn:
+        st.session_state.screen = 'player_compare_screen'
+        st.session_state.player_compare_list = player_compare_list
+        st.rerun()
+
+    # Screen to show comparison of players
+    elif st.session_state.screen == 'player_compare_screen':
+        player_compare_list = st.session_state.player_compare_list
+        player_compare_dict = {}
+        title_str = ''
+
+        # Loop to get data of players selected
+        for player_name in player_compare_list:
+            title_str = title_str + player_name + ' Vs '
+            with open('players_names.json') as rf:
+                data = json.load(rf)
+            player_full_name = data[player_name]
+
+            # Send API Request
+            response = requests.get('http://127.0.0.1:7000/player_record', params={'player': player_full_name}).json()
+            player_compare_dict[player_name] = response['Batting']['Overall Record']
+
+        # show dataframe and bar chart
+        player_compare_df = pd.DataFrame(player_compare_dict)
+        fig = px.bar(player_compare_df, x = player_compare_df.index, y = player_compare_df.columns, text_auto=True, barmode='group', log_y=True,
+                     height=600, width = 900, labels = {'index':'Stat'}, title = 'Head to Head of Players')
+        st.title(title_str[:-3])
+        st.dataframe(player_compare_df)
+        st.divider()
         st.plotly_chart(fig)
